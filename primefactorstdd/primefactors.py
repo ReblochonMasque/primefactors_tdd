6d9a1c67e0b6118b99478d00be09a007311b77b8
ReblochonMasque <trustgold@gmail.com>
2016-07-06 13:32:10 +0800

    Add specific tests for 2, 3 and 4 - Generalized function

--- primefactorstdd/primefactors.py
+++ primefactorstdd/primefactors.py
@@ -5,17 +5,24 @@ __author__ = 'Frederic Dupont'
 :License: GPL3
 """
 
+
 import sys
 
 
-def factor_of(num):
+def factors_of(num):
     """prime factors decompositon
     :param num: the number to decompose
     :return: a list of the prime factors of num
     """
-    return []
+    factors = []
+    if num  > 1:
+        if num % 2 == 0:
+            factors.append(2)
+            num = num // 2
+        if num > 1:
+            factors.append(num)
 
-
+    return factors
 
 
 def main(argv):
--- primefactorstdd/primefactorstests.py
+++ primefactorstdd/primefactorstests.py
@@ -6,19 +6,33 @@ __author__ = 'Frederic Dupont'
 """
 
 import unittest
-from primefactorstdd.primefactors import factor_of
+from primefactorstdd.primefactors import factors_of
 
 
 class TestPrimeFactors(unittest.TestCase):
     """test suite for PrimeFactors"""
 
-    def test_factors_of(self):
+    def test_factors_of_1(self):
         """test if factor_of(1) returns []
         """
-        self.assertEqual(factor_of(1), [])
+        self.assertEqual(factors_of(1), [])
 
 
+    def test_factors_of_2(self):
+        """test if factors_of(2) returns [2]
+        """
+        self.assertEqual(factors_of(2), [2])
 
+
+    def test_factors_of_3(self):
+        """test if factors_of(3) returns [3]
+        """
+        self.assertEqual(factors_of(3), [3])
+
+    def test_factors_of_4(self):
+        """test if factors_of(4) returns [2, 2]
+        """
+        self.assertEqual(factors_of(4), [2, 2])
 
 
 if __name__ == '__main__':
